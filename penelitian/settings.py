84fcdd7a68a8b0cf85a894a12d14dc80e9fab96a
Ghipar <ghifari983@gmail.com>
2023-12-08 19:45:50 +0700

    add vercel configuration

--- penelitian/settings.py
+++ penelitian/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^g!=@=1@gkqo5=rnj!qtjlsrk0^x+e875628wyzj1=)%v*m$5@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -117,9 +117,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_build', 'static')
+
+
 
 
 # Default primary key field type
--- penelitian/urls.py
+++ penelitian/urls.py
@@ -27,3 +27,6 @@ urlpatterns = [
     path('predictEn', views.predictEn, name='predictEn'),
     path('',views.index, name = 'homepage'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
